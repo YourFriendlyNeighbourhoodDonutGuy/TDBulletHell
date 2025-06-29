507abd5eeb72097f1249b7f3db9186754dc7e818
Jamie <Jamiedodger381@gmail.com>
2025-06-29 20:21:43 +0100

    project start

---
+++ build/CMakeFiles/TDBulletHell.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TDBulletHell.dir/link.d"
+  "CMakeFiles/TDBulletHell.dir/src/Main.cpp.o"
+  "CMakeFiles/TDBulletHell.dir/src/Main.cpp.o.d"
+  "TDBulletHell"
+  "TDBulletHell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TDBulletHell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
